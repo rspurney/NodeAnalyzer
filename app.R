3f2de95ddddde6ab1e0f5c83538e2ea14f77377e
rspurney <rspurney@gmail.com>
2020-06-09 11:23:13 -0400

    master: add instructions

--- app.R
+++ app.R
@@ -33,8 +33,15 @@ ui <- fluidPage(
       # Instructions
       headerPanel(""),
       h4("Instructions"),
-      p("Use the app"),
-      
+      p("First, to calculate necessary network statistics such as outdegree and indegree in 
+        Cytoscape, select Tools -> Analyze Network, check the Analyze as Directed Graph 
+        if applicable, and then press OK to perform the analysis."),
+      p("To export node and edge files from Cytoscape, select File -> Export -> Table
+        to File... and then choose default edge or default node in the 'Select a table
+        to export' dropdown. Press OK to export each file."),
+      p("Import the node and edge table files into the corresponding prompts above and 
+        then press the Run Analysis button to calculate impact scores. Results can be 
+        downloaded as a table using the Download Results button.")
       ),
     
     mainPanel(
@@ -76,7 +83,7 @@ server <- function(input, output) {
     scoringTable <<- calcRobustness(input$nodeTableFile, input$edgeTableFile) # <<- denotes global variable
     print("Finished analysis.")
     
-    # Show outputs
+    # Show output table and plots
     output$table <- renderTable(scoringTable, rownames = FALSE)
     output$impactPlot <- renderPlot({
       plot(scoringTable$Gene, scoringTable$Impact, xlab = "", ylab = "Impact", cex.axis = 0.75, las = 2)
