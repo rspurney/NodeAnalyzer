c7c403f45379cf1d7de1a77ea5485fe6cbe70964
rspurney <rspurney@gmail.com>
2020-06-24 20:25:05 -0400

    master: remove calcRobustness and run

--- run.R
+++
@@ -1,6 +0,0 @@
-library(shiny)
-
-# Change this path to your directory containing the app
-setwd("/Users/ryan/Documents/NCSU/Sozzani Lab/Robustness Analysis/RobustnessAnalyzer")
-source("calcRobustness.R")
-runApp("app.R")
